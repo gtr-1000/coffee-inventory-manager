a0b8be975628c0a217b6c26f2e8a42c07682a715
gtr-1000 <dalbertvinicius5@gmail.com>
2026-08-05 15:25:14 -0300

    Add event listener to button for list updates

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const input = document.getElementById("blend-input");
 const button = document.getElementById("add-btn");
 
 const list = document.getElementById("blend-list");
+
+button.addEventListener("click", () => {
+  const blendName = input.value;
+  const li = document.createElement("li");
+  li.textContent = blendName;
+  list.appendChild(li);
+});
